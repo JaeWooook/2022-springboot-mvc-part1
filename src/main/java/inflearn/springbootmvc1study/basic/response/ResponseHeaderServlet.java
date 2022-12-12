32dfa00202d1812357cb0dc8e7d3eafbbb38f176
br12345678 <mystyle730@gmail.com>
2022-12-12 23:00:57 +0900

    HttpServletResponse - 기본 사용법(추가)

--- src/main/java/inflearn/springbootmvc1study/basic/response/ResponseHeaderServlet.java
+++ src/main/java/inflearn/springbootmvc1study/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package inflearn.springbootmvc1study.basic.response;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,8 +25,11 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setHeader("my-header", "hello");//헤더에 hello가 실려간다,
 
         //[Header 편의 메서드]
-        content(response);
+//        content(response);
+//        cookie(response);
+//        redirect(response);
 
+        //[message body]
         PrintWriter writer = response.getWriter();
         writer.println("ok");
     }
@@ -33,5 +37,23 @@ public class ResponseHeaderServlet extends HttpServlet {
     private void content(HttpServletResponse response) {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
+//        response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good", "Max-Age=600"); //이렇게 해도된다.
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);// 쿠키 사용시간 600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code:302
+        //Location: /basic/hello-form.html
+
+//        response.setStatus(HttpServletResponse.SC_FOUND);
+//        response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html"); //위에 2줄보다 이렇게 한줄하면된다.
     }
 }
