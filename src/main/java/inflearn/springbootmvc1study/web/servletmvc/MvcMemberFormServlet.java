fafcce8c4e1885d5f9506228327cb3b8c6d9e158
br12345678 <mystyle730@gmail.com>
2022-12-19 00:20:34 +0900

    MVC 패턴 - 적용

---
+++ src/main/java/inflearn/springbootmvc1study/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,22 @@
+package inflearn.springbootmvc1study.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);//forward는 서버내부에서 호출하는것이라서 클라이언트가 인지하지못한다.
+        //redirect는 클 <-> 서 , 클 <-> 서 2번 호출하는것이라서 클라이언트가 인지할 수 있다.
+    }
+}
